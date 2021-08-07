4b62b75bbee6e3f1bd32f62d2cfd9c15aae93041
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-08-07 17:02:22 -0300

    Second commit

--- two_project/TwoScript.js
+++ two_project/TwoScript.js
@@ -1,1 +1,16 @@
-console.log('hello word')
+function Place(){
+    var nome = document.getElementById("nome").value;
+    var Email = document.getElementById("email").value;
+    var pass = document.getElementById("senha").value;
+
+    var txt = document.getElementById("campo").value;
+    txt[0] = nome;
+    console.log(txt);
+
+
+}
+
+
+
+
+console.log('hello word');
