6d135b729ad3bd34e7e4b957c08fa204700c5591
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-08-09 15:34:49 -0300

    Third Test student javascript

--- two_project/TwoScript.js
+++ two_project/TwoScript.js
@@ -1,4 +1,4 @@
-//Problem of Student is in JS
+//Declaration the variable with student of Javascript
 var Name;
 
 var eMail;
@@ -7,22 +7,37 @@ var PassWord;
 
 var num = 0;
 
-function escrever(){
-    info = "Usuário número" + (num++) + "/n";
-    info += "Name:" + Name + "/n";
-    info += "E-mail:" + eMail + "/n/n";
+//Date the today too declaration with student Javascript
+hoje = new Date();
+dia =  hoje.getDate()
+mes = hoje.getMonth() + 1
+ano = hoje.getYear() + 1900
+
+//go date today in screen
+document.write("<h3>Hoje:" + dia + '/' + mes + '/' + ano + "</h3>");
+
+// declaration of function basic with student Javascript
+function escrever(quadro){
+  info = "Usuário número" + (num++) + "/n";
+  info += "Name:" + Name + "/n";
+  info += "E-mail:" + eMail + "/n/n";
+  quadro.value += info
+  this.form.Area
 }
+
+window.status = "hello word";
+
 
 //Function is in the ok!!
 //event onclick in HTML
 function Place(){
-    var nome = document.getElementById("nome").value;
-    var Email = document.getElementById("email").value;
-    var pass = document.getElementById("senha").value;
+  var nome = document.getElementById("nome").value;
+  var Email = document.getElementById("email").value;
+  var pass = document.getElementById("senha").value;
 
-    var txt = document.getElementById("campo").value;
-    txt[0] = nome;
-    console.log(txt);
+  var txt = document.getElementById("campo").value;
+  txt[0] = nome;
+  console.log(txt);
 
 
 }
@@ -33,15 +48,12 @@ function Place(){
 console.log('hello word');
 
 /*
-
-█     █░ ██▓ ██▓     ██ ▄█▀▓█████  ██▀███  
-▓█░ █ ░█░▓██▒▓██▒     ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
-▒█░ █ ░█ ▒██▒▒██░    ▓███▄░ ▒███   ▓██ ░▄█ ▒
-░█░ █ ░█ ░██░▒██░    ▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  
-░░██▒██▓ ░██░░██████▒▒██▒ █▄░▒████▒░██▓ ▒██▒
-░ ▓░▒ ▒  ░▓  ░ ▒░▓  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
-  ▒ ░ ░   ▒ ░░ ░ ▒  ░░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
-  ░   ░   ▒ ░  ░ ░   ░ ░░ ░    ░     ░░   ░ 
-    ░     ░      ░  ░░  ░      ░  ░   ░     
-                                            
+ ____      ____  _____    _____      ___  ____    ________   _______     
+|_  _|    |_  _||_   _|  |_   _|    |_  ||_  _|  |_   __  | |_   __ \    
+  \ \  /\  / /    | |      | |        | |_/ /      | |_ \_|   | |__) |   
+   \ \/  \/ /     | |      | |   _    |  __'.      |  _| _    |  __ /    
+    \  /\  /     _| |_    _| |__/ |  _| |  \ \_   _| |__/ |  _| |  \ \_  
+     \/  \/     |_____|  |________| |____||____| |________| |____| |___| 
+                                                                         
+                                         
  */
